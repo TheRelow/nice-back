3beeae60a2cc183672d58cc27464ad745c9d5bad
Egor Antropov <dev.antropov@gmail.com>
2024-02-09 09:15:21 +0300

    new task resource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,35 +9,30 @@ import {UserRoles} from "@/features/roles/user-roles.model";
 import { AuthModule } from '@/auth/auth.module';
 import { PostsModule } from '@/features/posts/posts.module';
 import {Post} from "@/features/posts/posts.model";
+import {TasksModule} from "@/features/tasks/tasks.module";
 
-const imports: any = [
-    ConfigModule.forRoot({
+@Module({
+    controllers: [],
+    providers: [],
+    imports: [
+      ConfigModule.forRoot({
         envFilePath: `.${process.env.NODE_ENV}.env`
-    }),
-]
-console.log('process.env.DB', process.env.DB)
-if (process.env.DB !== 'OFF') {
-    imports.push(
+      }),
       SequelizeModule.forRoot({
-          dialect: 'postgres',
-          host: process.env.POSTGRES_HOST,
-          port: Number(process.env.POSTGRESS_PORT),
-          username: process.env.POSTGRES_USER,
-          password: process.env.POSTGRESS_PASSWORD,
-          database: process.env.POSTGRES_DB,
-          models: [User, Role, UserRoles, Post],
-          autoLoadModels: true,
+        dialect: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRESS_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRESS_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        models: [User, Role, UserRoles, Post],
+        autoLoadModels: true,
       }),
       UsersModule,
       RolesModule,
       AuthModule,
       PostsModule,
-      )
-}
-
-@Module({
-    controllers: [],
-    providers: [],
-    imports
+      TasksModule
+    ]
 })
 export class AppModule {}
